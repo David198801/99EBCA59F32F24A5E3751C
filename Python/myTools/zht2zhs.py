ae7b1504cd7505029b4998bbc8c91d5279eb1ad4
david <david@david.com>
2024-05-21 15:00:11 +0800

    1

--- Python/myTools/zht2zhs.py
+++
@@ -1,9 +0,0 @@
-#coding:utf-8
-import zhconv
-
-fileName="output.txt"
-with open(fileName,"r",encoding="utf8") as txt:
-    r = txt.read()
-    text = zhconv.convert(r, 'zh-hans')
-    with open(fileName+"_zhs.txt","w",encoding="utf8") as otxt:
-        otxt.write(text)
